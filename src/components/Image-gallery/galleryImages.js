21f80935a589dea640556a873de0792bc697f424
Bala <balapeesala1022@gmail.com>
2023-06-10 12:20:44 +0530

    Initial commit

---
+++ src/components/Image-gallery/galleryImages.js
@@ -0,0 +1,20 @@
+
+  const galleryImg01=<img src="/images/gallery-01.jpg" alt="" />
+  const galleryImg07=<img src="/images/gallery-07.jpg" alt=""/>
+  const galleryImg02=<img src="/images/gallery-02.jpg" alt="" />
+  const galleryImg06=<img src="/images/gallery-06.jpg"alt=""/>
+  const galleryImg03=<img src="/images/gallery-03.jpg" alt=""/>
+  const galleryImg04=<img src="/images/gallery-04.jpg" alt=""/>
+  const galleryImg05=<img src="/images/gallery-05.jpg" alt=""/>
+  const galleryImg08=<img src="/images/gallery-07.jpg" alt=""/>
+const galleryImages = [
+  galleryImg01,
+  galleryImg07,
+  galleryImg02,
+  galleryImg06,
+  galleryImg04,
+  galleryImg05,
+  galleryImg03
+]
+
+export default galleryImages;
